0b7234d0c6ef9e8299aedef09638d2b32e744803
Amit Finkelstein <amittein@gmail.com>
2026-05-02 22:37:58 +0300

    Allow asyncio slow task warnings in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -74,7 +74,7 @@ def pytest_runtest_call(item: pytest.Item) -> Any:
             for allowed_log in chain(
                 allowed_logs, ["We found a custom integration pima_force"]
             )
-        ):
+        ) or (message.startswith("Executing <Task") and message.endswith(" seconds")):
             continue
         pytest.fail(f"Disallowed {record.levelname} log: {message}")
 
